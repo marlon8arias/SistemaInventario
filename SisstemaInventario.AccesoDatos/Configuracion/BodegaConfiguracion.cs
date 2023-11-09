2d487016b6ca846fc4ef623640ff2d446c2303f2
marlon <marlon8arias@gmail.com>
2023-11-08 22:56:47 -0600

    Configuración del proyecto, migración de base de datos y uso de Fluent API

---
+++ SisstemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisstemaInventario.AccesoDatos.Configuracion
+{
+    public class BodegaConfiguracion : IEntityTypeConfiguration<Bodega>
+    {
+        public void Configure(EntityTypeBuilder<Bodega> builder)
+        {
+            builder.Property(X => X.Id).IsRequired();
+            builder.Property(X => X.Nombre).IsRequired().HasMaxLength(60);
+            builder.Property(X => X.Descripcion).IsRequired().HasMaxLength(100);
+            builder.Property(X => X.Estado).IsRequired();
+        }
+    }
+}
