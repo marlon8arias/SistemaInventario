8d5ff89860a77a5fb54dff4b5a527dd437550ed6
marlon <marlon8arias@gmail.com>
2024-08-20 19:21:57 -0600

    Configuración del proyecto, creando bodegas y ejecutando migraciones

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
+
+namespace SistemaInventario.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Agregando Propiedad DbSet
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        // Agregando fluentAPI
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    // Por default la clase se crea como internal, pero la cambiamos a public.
+    public class Bodega 
+    {
+        // Utilizando data-notations
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre debe contener máximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [MaxLength(100, ErrorMessage = "la descripcón debe contener máximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        [MaxLength(1, ErrorMessage = "El estado solo puede contener 1 caracter")]
+        public bool Estado { get; set; }
+    }
+}
